4e340f272684c0e63ff896f549521ad885bfe437
naughtydc <naughtdc5039@gmail.com>
2020-12-15 15:47:49 +0530

    i have learnt constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,6 +17,9 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
+
+    log6 = new Log(100,100,80,PI/2);
 
 
     ground = new Ground(600,height,1200,20);
@@ -35,6 +41,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    chain1 = new chain(bird.body,log6.body);
 
 }
 
@@ -58,7 +66,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    chain1.display();
 }
